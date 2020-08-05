8771589571a8b48919f814d12469ff77de1bed09
Kai Labusch <labusch@gmail.com>
2020-08-05 10:31:46 +0200

    refactor detection

--- detect.py
+++ detect.py
@@ -6,12 +6,9 @@ from utils.datasets import *
 
 import click
 from tqdm import tqdm
-from PIL import Image
 
 import torch
 from torch.utils.data import DataLoader
-# from torchvision import datasets
-# from torch.autograd import Variable
 
 import pandas as pd
 
@@ -22,15 +19,14 @@ import pandas as pd
 @click.argument('weights_path', type=click.Path(exists=True))
 @click.argument('result_file', type=click.Path())
 # @click.argument('class_path', type=click.Path(exists=True))
-@click.option('--batch-size', type=int, default=128, help="size of the batches")
+@click.option('--batch-size', type=int, default=16, help="size of the batches")
 @click.option("--img-size", type=int, default=416, help="size of each image dimension")
 @click.option('--n-cpu', type=int, default=4, help="number of cpu threads to use during batch generation")
 @click.option("--conf-thres", type=float, default=0.01, help="object confidence threshold")
-@click.option("--nms-thres", type=float, default=0.4, help="iou threshold for non-maximum suppression")
-@click.option('--perform-nms', type=bool, is_flag=True, default=False,
-              help="add if you actually want non-maximum supression")
-def cli(image_folder, model_def, weights_path, result_file, batch_size, img_size, n_cpu,
-        conf_thres, nms_thres, perform_nms):
+# @click.option("--nms-thres", type=float, default=0.4, help="iou threshold for non-maximum suppression")
+# @click.option('--perform-nms', type=bool, is_flag=True, default=False,
+#              help="add if you actually want non-maximum supression")
+def cli(image_folder, model_def, weights_path, result_file, batch_size, img_size, n_cpu, conf_thres):
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -63,6 +59,7 @@ def cli(image_folder, model_def, weights_path, result_file, batch_size, img_size
     progress = tqdm(enumerate(dataloader), total=len(dataloader))
 
     num_found = 0
+    check_point = 10000
 
     for batch_i, (img_paths, input_imgs, img_sizes) in progress:
 
@@ -111,24 +108,6 @@ def cli(image_folder, model_def, weights_path, result_file, batch_size, img_size
         detections['box_w'] = detections.x2 - detections.x1
         detections['box_h'] = detections.y2 - detections.y1
 
-        # if perform_nms:
-        #    output = non_max_suppression(output, conf_thres, nms_thres)
-
-        # for path, img, detections in zip(img_paths, input_imgs, output):
-        #
-        #     # img = np.array(Image.open(path))
-        #
-        #     # Draw bounding boxes and labels of detections
-        #     if detections is not None:
-        #         # Rescale boxes to original image
-        #         detections = rescale_boxes(detections, img_size, img.shape[:2])
-        #
-        #         for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
-        #             box_w = x2 - x1
-        #             box_h = y2 - y1
-        #
-        #             results.append((path, x1, y1, box_w, box_h, conf, cls_conf, cls_pred))
-
         num_found += len(detections)
 
         progress.set_description("Num found: {}".format(num_found))
@@ -139,15 +118,8 @@ def cli(image_folder, model_def, weights_path, result_file, batch_size, img_size
         del input_ten
         del output
 
-        if num_found > 0 and num_found % 10000 == 0:
+        if num_found > check_point:
             pd.concat(results).to_pickle(result_file)
-
-        # Save image and detections
-        # imgs.extend(img_paths)
-        # img_detections.extend(detections)
-
-    # result = pd.DataFrame(results, columns=['path', 'x', 'y', 'width', 'height', 'conf', 'cls_conf', 'cls_pred'])
-
-    # results = pd.concat(results)
+            check_point = num_found + 10000
 
     pd.concat(results).to_pickle(result_file)
